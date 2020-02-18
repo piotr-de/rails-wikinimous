18cdc1a533f7804592f90242aac370a602d0f05c
Piotr Damer <piotrd@czuby.net>
2020-02-18 15:08:22 +0000

    Created a basic wiki app

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,36 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to root_path
+  end
+
+  def edit; end
+
+  def update
+    @article.update(article_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
